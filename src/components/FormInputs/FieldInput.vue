450008cb767034bed13216b2e633cbd9ae2fd78d
BornaMarin <bornamarin3232@gmail.com>
2022-08-06 12:22:24 +0200

    feat: form inputs

---
+++ src/components/FormInputs/FieldInput.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="field">
+    <!--    <label v-if="label" class="field-label" :for="name">{{ label }}</label>-->
+    <input
+      class="field-input"
+      :id="id"
+      :type="type"
+      :disabled="disabled"
+      :name="name"
+      :placeholder="placeholder"
+      :aria-label="ariaLabel"
+      autocomplete="off"
+      @focus="toggleFocus"
+      @blur="toggleFocus"
+      v-model="computedValue"
+    />
+  </div>
+</template>
+
+<script>
+import BaseInputMixin from "@/mixins/BaseInputMixin";
+export default {
+  mixins: [BaseInputMixin],
+  methods: {
+    toggleFocus() {
+      this.$emit("focused");
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.field {
+  display: flex;
+  flex-direction: column;
+  &-label {
+  }
+  &-input {
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    width: 100%;
+    display: block;
+    color: white;
+    &:focus {
+      border: none;
+      outline: none;
+    }
+  }
+}
+</style>
---
+++ src/components/FormInputs/TextAreaInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <label :for="name">{{ label }}</label>
+    <textarea :id="id" :name="name" v-model="computedValue" />
+  </div>
+</template>
+
+<script>
+import BaseInputMixin from "@/mixins/BaseInputMixin";
+export default {
+  mixins: [BaseInputMixin],
+};
+</script>
