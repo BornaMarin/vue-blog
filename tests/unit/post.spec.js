fde663bf435b3b954b6f30bcf014e47979053965
BornaMarin <bornamarin3232@gmail.com>
2022-08-07 17:38:45 +0200

    feat: overall

---
+++ tests/unit/post.spec.js
@@ -0,0 +1,24 @@
+import { mount } from "@vue/test-utils";
+import PostItem from "@/components/ListItems/PostItem";
+import { capitalize } from "@/helpers/HelperFunctions";
+describe("CommentItem.vue", () => {
+  it("renders props when passed", async () => {
+    const props = {
+      post: {
+        title: "title",
+        body: "body",
+      },
+      user: {
+        name: "name",
+        username: "username",
+      },
+    };
+    const wrapper = mount(PostItem, {
+      propsData: props,
+    });
+    expect(wrapper.text()).toContain(capitalize(props.post.title));
+    expect(wrapper.text()).toContain(props.post.body);
+    expect(wrapper.text()).toContain(props.user.name);
+    expect(wrapper.text()).toContain(props.user.username);
+  });
+});
